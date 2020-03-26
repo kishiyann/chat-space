7bdf48e52cc787355b727d12341ee4719a30450c
kishiyann <to5hi5_0315@yahoo.co.jp>
2020-03-26 11:50:05 +0900

    変更しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root "messages#index"
   devise_for :users
-  resources :users, only: [:edit, :update, :destroy]
+  root 'groups#index'
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
